e939e669b6f4d658681bb377a51af8b0c26893dd
Eliel Gomes <elielgomespg@hotmail.com>
2022-10-19 12:13:49 -0300

    Teste routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AllRoutes from './routes'
+import App from './routes'
 import './style/global.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AllRoutes/>
+    <App/>
   </React.StrictMode>
 )
--- src/routes.jsx
+++ src/routes.jsx
@@ -6,22 +6,42 @@ import PizzaSalgada from "./pages/Pizza-Salgada";
 import PizzaDoce from "./pages/Pizza-Doce";
 import Porcao from "./pages/Porcao";
 import Drink from "./pages/Drink";
-import NotFound from "./pages/NotFound";
+// import NotFound from "./pages/404";
 
-const AllRoutes = () => {
+
+function App(){
   return (
     <BrowserRouter>
       <Routes>
+        
         <Route path='/' element={<Home/>} />
         <Route path='/pizzas-salgadas' element={<PizzaSalgada/>}/>
         <Route path='/pizzas-doces' element={<PizzaDoce/>}/>
         <Route path='/porcoes' element={<Porcao/>}/>
         <Route path='/drinks' element={<Drink/>}/>
-        <Route path='*' element={<NotFound/>}/>
+        <Route path='*' element={<NotFound404/>}/>
+
       </Routes>
     </BrowserRouter>
   );
 };
 
+function NotFound404() {
+  return (
+    <div>
+      <Header />
 
-export default AllRoutes;
+      <div id="container-notfound">
+        <div id="container-image">
+          <img id="image-404" src="/assets/not-found/404.png" alt="Erro 404" />
+        </div>
+
+        <h1 id="text-notfound">Page Not Found!</h1>
+      </div>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
