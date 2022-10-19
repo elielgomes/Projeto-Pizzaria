79dffd3b09d9843337d69ab928816ede643b2b4b
Eliel Gomes <elielgomespg@hotmail.com>
2022-10-19 13:32:03 -0300

    Alterações e Correcao no diretorio da rota NotFound

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,52 +1,36 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 
 import Home from "./pages/Home";
 import PizzaSalgada from "./pages/Pizza-Salgada";
 import PizzaDoce from "./pages/Pizza-Doce";
 import Porcao from "./pages/Porcao";
 import Drink from "./pages/Drink";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import "./style.css"
 
-// import NotFound from "./pages/404";
+import NotFound from "./pages/404";
 
 
 function App(){
+
   return (
+
     <BrowserRouter>
       <Routes>
         
-        <Route path='/' element={<Home/>} />
-        <Route path='/pizzas-salgadas' element={<PizzaSalgada/>}/>
-        <Route path='/pizzas-doces' element={<PizzaDoce/>}/>
-        <Route path='/porcoes' element={<Porcao/>}/>
-        <Route path='/drinks' element={<Drink/>}/>
-        <Route path='/*' element={<NotFound404/>}/>
+        <Route path='/' >
+          <Route index element={<Home/>}/>
+          <Route path='pizzas-salgadas' element={<PizzaSalgada/>}/>
+          <Route path='pizzas-doces' element={<PizzaDoce/>}/>
+          <Route path='porcoes' element={<Porcao/>}/>
+          <Route path='drinks' element={<Drink/>}/>
+          <Route path='*' element={<NotFound/>}/>
+        </Route>
 
       </Routes>
     </BrowserRouter>
+
   );
-};
 
-function NotFound404() {
-
-  return (
-    <div>
-      <Header />
-
-      <div id="container-notfound">
-        <div id="container-image">
-          <img id="image-404" src="/assets/not-found/404.png" alt="Erro 404" />
-        </div>
-
-        <h1 id="text-notfound">Page Not Found!</h1>
-      </div>
-
-      <Footer />
-    </div>
-  );
 };
 
 export default App;
